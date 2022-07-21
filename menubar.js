ed4ca15475ef7757440550dda5ec1613dfce4fa1
sw08 <lswja6866@naver.com>
2022-07-21 16:53:38 +0900

    오타 수정

--- menubar.js
+++ menubar.js
@@ -1,16 +1,15 @@
-
 window.onload = () => {
     var url
     var sources = {}
-    var sources = {header: null, footer: null}
     var source = document.getElementsByTagName('body')[0].innerHTML
     var request = new XMLHttpRequest()
     url = 'https://yswysw.kro.kr/footer.html'
     request.open('GET', url, true)
     request.onload = function () {
-        sources['footer'] = request2.responseText
+        sources['footer'] = request.responseText
     }
     request.send();
+
     var request2 = new XMLHttpRequest()
     url = 'https://yswysw.kro.kr/header.html'
     request2.open('GET', url, true);
@@ -18,5 +17,6 @@ window.onload = () => {
         sources['header'] = request2.responseText
     }
     request2.send();
-    document.getElementsByTagName('body')[0].innerHTML = sources.header + '\n' + source + '\n' + sources.footer     
+    console.log(sources, sources.header, sources.footer)
+    document.getElementsByTagName('body')[0].innerHTML = sources['header'] + '\n' + source + '\n' + sources['footer']
 }
