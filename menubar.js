347dac19fccec268cc74d53e4455883091059894
sw08 <lswja6866@naver.com>
2022-07-21 17:06:11 +0900

    푸터/헤더 조회 문제 해결

--- menubar.js
+++ menubar.js
@@ -3,20 +3,24 @@ window.onload = () => {
     var sources = {}
     var source = document.getElementsByTagName('body')[0].innerHTML
     var request = new XMLHttpRequest()
+    function getHeader() {
+        function editHTML() {
+            document.getElementsByTagName('body')[0].innerHTML = sources['header'] + '\n' + source + '\n' + sources['footer']
+        }
+        var request = new XMLHttpRequest()
+        url = 'https://yswysw.kro.kr/header.html'
+        request.open('GET', url, true);
+        request.onload = function () {
+            sources['header'] = request.responseText
+        }
+        request.onloadend = editHTML
+        request.send();
+    }
     url = 'https://yswysw.kro.kr/footer.html'
     request.open('GET', url, true)
     request.onload = function () {
         sources['footer'] = request.responseText
     }
+    request.onloadend = getHeader
     request.send();
-
-    var request2 = new XMLHttpRequest()
-    url = 'https://yswysw.kro.kr/header.html'
-    request2.open('GET', url, true);
-    request2.onload = function () {
-        sources['header'] = request2.responseText
-    }
-    request2.send();
-    console.log(sources, sources.header, sources.footer)
-    document.getElementsByTagName('body')[0].innerHTML = sources['header'] + '\n' + source + '\n' + sources['footer']
 }
