04bb8fb8c6741cde596b92d6088de1b9cc766ab7
sw08 <lswja6866@naver.com>
2022-07-22 13:20:26 +0900

    메인페이지 일부제작+수정

--- setup.js
+++ setup.js
@@ -1,26 +1,14 @@
-window.onload = () => {
+window.onload = function () {
     var url
-    var sources = {}
+    var header;
     var source = document.body.innerHTML
     var request = new XMLHttpRequest()
-    function getHeader() {
-        function editHTML() {
-            document.body.innerHTML = "<div id='wrapper'>\n" + sources['header'] + "\n<div id='container'>\n" + source + "</div>\n</div>"
-        }
-        var request = new XMLHttpRequest()
-        url = 'header.html'
-        request.open('GET', url, true);
-        request.onload = function () {
-            sources['header'] = request.responseText
-        }
-        request.onloadend = editHTML
-        request.send();
-    }
-    url = 'footer.html'
-    request.open('GET', url, true)
+    request.open('GET', 'header.html', true)
     request.onload = function () {
-        sources['footer'] = request.responseText
+        header = request.responseText
     }
-    request.onloadend = getHeader
-    request.send();
+    request.onloadend = function () {
+        document.body.innerHTML = "<div id='wrapper'>\n" + header + "\n<div id='container'>\n" + source + "</div>\n</div>"
+    }
+    request.send()
 }
