bc3b6d6c82cf6ddd886e69cef06bebd9bb9516ad
sw08 <lswja6866@naver.com>
2022-07-24 18:19:33 +0900

    푸터 생성

--- setup.js
+++ setup.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     var url
     var header;
+    var footer;
     var source = document.body.innerHTML
     var request = new XMLHttpRequest()
     request.open('GET', 'header.html', true)
@@ -8,7 +9,15 @@ window.onload = function () {
         header = request.responseText
     }
     request.onloadend = function () {
-        document.body.innerHTML = "<div id='wrapper'>\n" + header + "\n<div id='container'>\n" + source + "</div>\n</div>"
+        var request = new XMLHttpRequest()
+        request.open('GET', 'footer.html', true)
+        request.onload = function () {
+            footer = request.responseText
+        }
+        request.onloadend = function () {
+            document.body.innerHTML = "<div id='wrapper'>\n" + header + "\n<div id='container'>\n" + source + "</div>\n" + footer + "\n</div>"
+        }
+        request.send()
     }
     request.send()
 }
